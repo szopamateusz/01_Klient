98da0f6815bde3c327de6f1ce0d615bce039c1a2
szopamateusz <github@mateusz-szopa.pl>
2017-02-26 15:28:21 +0100

    Add files via upload

---
+++ 01_Serwer - asynhronicznie/01_Serwer/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace _01_Serwer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.nrPort = new System.Windows.Forms.NumericUpDown();
+            this.btStart = new System.Windows.Forms.Button();
+            this.btStop = new System.Windows.Forms.Button();
+            this.lbMessages = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nrPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(12, 12);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(45, 17);
+            this.lblAddress.TabIndex = 0;
+            this.lblAddress.Text = "Adres";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(63, 12);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(213, 22);
+            this.txtAddress.TabIndex = 1;
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(282, 12);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(34, 17);
+            this.lblPort.TabIndex = 2;
+            this.lblPort.Text = "Port";
+            // 
+            // nrPort
+            // 
+            this.nrPort.Location = new System.Drawing.Point(322, 12);
+            this.nrPort.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.nrPort.Name = "nrPort";
+            this.nrPort.Size = new System.Drawing.Size(120, 22);
+            this.nrPort.TabIndex = 3;
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(15, 271);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(154, 41);
+            this.btStart.TabIndex = 5;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // btStop
+            // 
+            this.btStop.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btStop.Location = new System.Drawing.Point(288, 271);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(154, 41);
+            this.btStop.TabIndex = 6;
+            this.btStop.Text = "Stop";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
+            // 
+            // lbMessages
+            // 
+            this.lbMessages.FormattingEnabled = true;
+            this.lbMessages.ItemHeight = 16;
+            this.lbMessages.Location = new System.Drawing.Point(15, 40);
+            this.lbMessages.Name = "lbMessages";
+            this.lbMessages.Size = new System.Drawing.Size(427, 212);
+            this.lbMessages.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 341);
+            this.Controls.Add(this.lbMessages);
+            this.Controls.Add(this.btStop);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.nrPort);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.lblAddress);
+            this.Name = "Form1";
+            this.Text = "TCP Serwer (01)";
+            ((System.ComponentModel.ISupportInitialize)(this.nrPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.NumericUpDown nrPort;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.ListBox lbMessages;
+    }
+}
+
---
+++ 01_Serwer - asynhronicznie/01_Serwer/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace _01_Serwer
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener server;
+        private TcpClient client;
+        private delegate void SetTextCallBack(string text);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            IPAddress adresIP;
+            try
+            {
+                adresIP = IPAddress.Parse(txtAddress.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Błędny format adresu IPv4", "Błąd");
+                txtAddress.Text = String.Empty;
+                return;
+            }
+            int port = Convert.ToInt32(nrPort.Value);
+            try
+            {
+                server = new TcpListener(adresIP, port);
+                server.Start();
+                //client = server.AcceptTcpClient();
+                //lbMessages.Items.Add("Nawiązano połączenie : " + client.Client.RemoteEndPoint.ToString());
+                server.BeginAcceptTcpClient(new AsyncCallback(AcceptTcpClientCallBack), server);
+                btStart.Enabled = false;
+                btStop.Enabled = true;
+                //client.Close();
+                //server.Stop();
+            }
+            catch (Exception ex)
+            {
+                lbMessages.Items.Add("Błąd inicjacji serwera ! ");
+                MessageBox.Show(ex.ToString(), "Błąd");
+            }
+        }
+        private void AcceptTcpClientCallBack(IAsyncResult AsyncResult) {
+            TcpListener s= (TcpListener)AsyncResult.AsyncState;
+            client = s.EndAcceptTcpClient(AsyncResult);
+            SetListBoxText("Połącznie powiodło się");
+            client.Close();
+            server.Stop();
+        }
+        private void SetListBoxText(string text) {
+            if (lbMessages.InvokeRequired)
+            {
+                SetTextCallBack f = new SetTextCallBack(SetListBoxText);
+                this.Invoke(f, new object[] { text });
+            }
+            else {
+                lbMessages.Items.Add(text);
+            }        
+        }
+        private void btStop_Click(object sender, EventArgs e)
+        {
+            client.Close();
+            server.Stop();
+            lbMessages.Items.Add("Zakończono pracę serwera");
+            btStop.Enabled = false;
+            btStart.Enabled = true;
+        }
+    }
+}
